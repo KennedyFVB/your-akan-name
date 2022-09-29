910f79fb41fb1833003468464b3b28e13d17b6d5
KennedyFVB <kenkahura@gmail.com>
2022-09-29 18:19:52 +0300

    Input prompts added

--- js/main.js
+++ js/main.js
@@ -27,7 +27,21 @@ const DAYS_OF_THE_WEEK = [
 ];
 
 // get user input
-    //get user's birthday
-    //get user's gender
+//get user's birthday
+//get user's gender
 
 // calculate day of birth
+var day = prompt("Enter your day of birth in DD format: ");
+var month = prompt("Enter your month of birth in MM format: ");
+var year = prompt("Enter your year of birth in YYYY format: ");
+var gender = prompt("Enter your gender in M/F format: ");
+var century = year.slice(0, 2);
+
+var dayOfTheWeek =
+  (century / 4 -
+    2 * parseint(century) -
+    1 +
+    (5 * parseint(year)) / 4 +
+    (26 * (parseint(month) + 1)) / 10 +
+    parseint(day)) %
+  7;
