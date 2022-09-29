cefd7075178db973a3ad7842e8974a9ec90d8bd2
KennedyFVB <kenkahura@gmail.com>
2022-09-29 15:59:34 +0300

    constants declared as lists

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const MALE_NAMES = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+];
+const FEMALE_NAMES = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama",
+];
+const DAYS_OF_THE_WEEK = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+// get user input
+    //get user's birthday
+    //get user's gender
+
+// calculate day of birth
