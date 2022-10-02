5bb94d76c5d79c806fa098f7ea63a68d93334bc7
KennedyFVB <kenkahura@gmail.com>
2022-10-02 23:56:20 +0300

    Debugging day you were born

--- js/main.js
+++ js/main.js
@@ -73,13 +73,13 @@ if (dayOfWeekNumber == 0){
  console.log(index);
 
 if (myGenderValue == "male" && monthValid && dayValid) {
-  document.getElementById('result').textContent = "You were born on a " + dayOfWeekNumber[index] + " , your Akan name is " + maleAkanNames[index];
+  document.getElementById('result').textContent = "You were born on a " + daysOfWeek[index] + " , your Akan name is " + maleAkanNames[index];
   document.getElementById('display-name').textContent = "Here is your Akan name: ";
   document.getElementById('result').style.fontSize = "18px";
   document.querySelector('h1').textContent = "Hello" + " " + maleAkanNames[index];
   return false;
 } else if (myGenderValue == "female" && monthValid && dayValid) {
-  document.getElementById('result').textContent = "You were born on a " + dayOfWeekNumber[index] + " , your Akan name is " + femaleAkanNames[index];
+  document.getElementById('result').textContent = "You were born on a " + daysOfWeek[index] + " , your Akan name is " + femaleAkanNames[index];
   document.getElementById('display-name').textContent = "Here is your Akan name: ";
   document.getElementById('result').style.fontSize = "18px";
   document.querySelector('h1').textContent = "Hello" + " " + femaleAkanNames[index];
