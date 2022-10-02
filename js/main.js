0ca0709fd30278d791b721c020df019dfbc8dc33
KennedyFVB <kenkahura@gmail.com>
2022-10-02 18:37:14 +0300

    Input button added

--- js/main.js
+++ js/main.js
@@ -31,17 +31,61 @@ const DAYS_OF_THE_WEEK = [
 //get user's gender
 
 // calculate day of birth
-var day = prompt("Enter your day of birth in DD format: ");
-var month = prompt("Enter your month of birth in MM format: ");
-var year = prompt("Enter your year of birth in YYYY format: ");
-var gender = prompt("Enter your gender in M/F format: ");
-var century = year.slice(0, 2);
 
-var dayOfTheWeek =
-  (century / 4 -
-    2 * parseint(century) -
-    1 +
-    (5 * parseint(year)) / 4 +
-    (26 * (parseint(month) + 1)) / 10 +
-    parseint(day)) %
-  7;
+function getUserInput() {
+  var day = prompt("Enter your day of birth in DD format: ");
+  var month = prompt("Enter your month of birth in MM format: ");
+  var year = prompt("Enter your year of birth in YYYY format: ");
+  var gender = prompt("Enter your gender in M/F format: ");
+  var century = year.slice(0, 2);
+}
+
+// var dayOfTheWeek =
+//   (parseint(century) / 4 -
+//     2 * parseint(century) -
+//     1 +
+//     (5 * parseint(year)) / 4 +
+//     (26 * (parseint(month) + 1)) / 10 +
+//     parseint(day)) %
+//   7;
+
+// var indexOfDay = DAYS_OF_THE_WEEK.indexOfDay(dayOfTheWeek);
+// var name = null;
+
+// if (gender.toLocaleLowerCase().startsWith("m") === "m") {
+//   name = MALE_NAMES[indexOfDay];
+// } else if (gender.toLocaleLowerCase().startsWith("f")) {
+//   name = FEMALE_NAMES[indexOfDay];
+// } else {
+//   console.log("Invalid gender");
+// }
+
+function getAkanName(day, month, year, gender) {
+  var dayOfTheWeek =
+    (parseint(century) / 4 -
+      2 * parseint(century) -
+      1 +
+      (5 * parseint(year)) / 4 +
+      (26 * (parseint(month) + 1)) / 10 +
+      parseint(day)) %
+    7;
+
+  var indexOfDay = DAYS_OF_THE_WEEK.indexOfDay(dayOfTheWeek);
+  var name = null;
+
+  if (gender.toLocaleLowerCase().startsWith("m") === "m") {
+    name = MALE_NAMES[indexOfDay];
+  } else if (gender.toLocaleLowerCase().startsWith("f")) {
+    name = FEMALE_NAMES[indexOfDay];
+  } else {
+    console.log("Invalid gender");
+  }
+
+  return name;
+}
+
+function main() {
+  getUserInput();
+  var userAkanName = getAkanName(day, month, year, gender);
+  console.log("Your name is " + userAkanName);
+}
